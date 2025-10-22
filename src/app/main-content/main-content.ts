395ec566bb06cc3969b77a8f0b132e3d21974b4f
TimDaubner <tim.daubner@outlook.com>
2025-10-22 10:42:41 +0200

    added router components(main-content,imprint)

--- src/app/main-content/main-content.ts
+++ src/app/main-content/main-content.ts
@@ -1,8 +1,14 @@
 import { Component } from '@angular/core';
+import { Aot } from './aot/aot';
+import { AboutMe } from './about-me/about-me';
+import { MySkills } from './my-skills/my-skills';
+import { MyProjects } from './my-projects/my-projects';
+import { TeamComments } from './team-comments/team-comments';
+import { ContactMe } from './contact-me/contact-me';
 
 @Component({
   selector: 'app-main-content',
-  imports: [],
+  imports: [Aot,AboutMe,MySkills,MyProjects,TeamComments,ContactMe],
   templateUrl: './main-content.html',
   styleUrl: './main-content.scss'
 })
