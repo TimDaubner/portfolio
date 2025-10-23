217fadaa68487c507eb26be3bde2791fee8f0c17
TimDaubner <tim.daubner@outlook.com>
2025-10-23 18:01:10 +0200

    arrow hover effect

--- src/app/main-content/aot/aot.ts
+++ src/app/main-content/aot/aot.ts
@@ -7,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrl: './aot.scss'
 })
 export class Aot {
-  imgSrc!:string;
+  arrowbtn:string = "assets/imgs/arrow-down.svg";
+  toggleImg(entered:boolean){
+    if(entered){
+      this.arrowbtn ="assets/imgs/arrow-down-hover.svg";
+      console.log("hover enter");
+      
+    }
+    else{
+      this.arrowbtn ="assets/imgs/arrow-down.svg";
+      console.log("hover exit");
+      
+    }
+  }
 }
