fb0d799ad1fec5f0291417d96b444973d87f84dd
TimDaubner <tim.daubner@outlook.com>
2025-10-23 16:35:36 +0200

    added font + design aot

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MainContent } from './main-content/main-content';
 import { Header } from "./shared/header/header";
 import { Footer } from "./shared/footer/footer";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MainContent, Header, Footer],
+  imports: [RouterOutlet, Header, Footer],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
--- src/app/main-content/aot/aot.ts
+++ src/app/main-content/aot/aot.ts
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrl: './aot.scss'
 })
 export class Aot {
-
+  imgSrc!:string;
 }
