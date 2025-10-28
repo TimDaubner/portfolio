6358ac8c3d568af284d41d99a9938b7283ac067c
TimDaubner <tim.daubner@outlook.com>
2025-10-28 18:03:55 +0100

    added icons for about-me and my-skills component

--- src/app/main-content/my-skills/my-skills.ts
+++ src/app/main-content/my-skills/my-skills.ts
@@ -12,53 +12,50 @@ export class MySkills {
   skills: {
     name: string;
     image: string;
-  }[] = [{
-    name: "Angular",
-    image: "",
-  },
-{
-    name: "Angular",
-    image: "",
-  },
-  {
-    name: "TypeScript",
-    image: "",
-  },
-  {
-    name: "JavaScript",
-    image: "",
-  },
-  {
-    name: "HTML",
-    image: "",
-  },
-  {
-    name: "CSS",
-    image: "",
-  },
-  {
-    name: "REST-API",
-    image: "",
-  },
-  {
-    name: "Firebase",
-    image: "",
-  },
-  {
-    name: "Git",
-    image: "",
-  },
-  {
-    name: "Scrum",
-    image: "",
-  },
-  {
-    name: "Material design",
-    image: "",
-  },
-  {
-    name: "Challenge me",
-    image: "",
-  },
-]
+  }[] = [
+      {
+        name: "Angular",
+        image: "assets/imgs/my-skills/angular_icon.svg",
+      },
+      {
+        name: "TypeScript",
+        image: "assets/imgs/my-skills/typescript_icon.svg",
+      },
+      {
+        name: "JavaScript",
+        image: "assets/imgs/my-skills/javascript_icon.svg",
+      },
+      {
+        name: "HTML",
+        image: "assets/imgs/my-skills/HTML_icon.svg",
+      },
+      {
+        name: "CSS",
+        image: "assets/imgs/my-skills/css_icon.svg",
+      },
+      {
+        name: "REST-API",
+        image: "assets/imgs/my-skills/API_icon.svg",
+      },
+      {
+        name: "Firebase",
+        image: "assets/imgs/my-skills/firebase_icon.svg",
+      },
+      {
+        name: "Git",
+        image: "assets/imgs/my-skills/git_icon.svg",
+      },
+      {
+        name: "Scrum",
+        image: "assets/imgs/my-skills/scrum_icon.svg",
+      },
+      {
+        name: "Material design",
+        image: "assets/imgs/my-skills/materialdesign_icon.svg",
+      },
+      {
+        name: "Challenge me",
+        image: "assets/imgs/my-skills/challengeme_icon.svg",
+      },
+    ]
 }
