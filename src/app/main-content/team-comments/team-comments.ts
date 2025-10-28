4debe9891d2378dfc38aba6663a8c883c550ddba
TimDaubner <tim.daubner@outlook.com>
2025-10-28 20:37:13 +0100

    style for contact me start and other components

--- src/app/main-content/team-comments/team-comments.ts
+++ src/app/main-content/team-comments/team-comments.ts
@@ -13,19 +13,19 @@ export class TeamComments {
     comment: string
   }[] = [
       {
-        name: "",
-        project_name: "",
-        comment: ""
+        name: "Noah Mueller",
+        project_name: "Project Jump Quest",
+        comment: "‘’Marcus had to develop, format and deliver content in collaboration with the team members.He is a reliable and friendly person.’’"
       },
       {
-        name: "",
-        project_name: "",
-        comment: ""
+        name: "Evelyn Marx",
+        project_name: "Project Pollo Loco",
+        comment: "‘’ He is a trustworthy teamplayer and can cope with the stress of deadlines. Structured work and clear code. ‘’"
       },
       {
-        name: "",
-        project_name: "",
-        comment: ""
+        name: "James Rugman",
+        project_name: "Project Join",
+        comment: "‘’ He is a reliable and friendly person. Work in a structured way and write a clear code. I recommend him as a colleague.’’"
       },
     ]
 }
