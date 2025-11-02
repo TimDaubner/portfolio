31a774613acf79f140c4d128b03ac82fb5c48a26
TimDaubner <tim.daubner@outlook.com>
2025-11-02 19:52:18 +0100

    push input field

--- src/app/main-content/contact-me/input/input.ts
+++ src/app/main-content/contact-me/input/input.ts
@@ -1,11 +1,35 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-input',
-  imports: [],
+  imports: [FormsModule],
+  standalone: true,
   templateUrl: './input.html',
   styleUrl: './input.scss'
 })
 export class Input {
+  contactData: {
+    name: string,
+    email: string,
+    message: string
+  } = {
+      name: "",
+      email: "",
+      message: ""
+    }
 
+  submitted = false;
+
+  onSubmit(ngForm: NgForm) {
+    this.submitted = true;
+    if (ngForm.valid && ngForm.submitted) {
+      console.log(this.contactData);
+    }
+  }
+
+  onInputChange(){
+    console.log("changed");
+    
+  }
 }
